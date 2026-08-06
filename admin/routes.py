ae295396f1c1af2ef2836e152f13a77b2cd78ef9
EpiksXX <didired284@gmail.com>
2026-08-06 22:01:43 +0300

    routes.py

--- admin/routes.py
+++ admin/routes.py
@@ -1,1 +1,14 @@
+from flask import Blueprint, render_template
 
+admin = Blueprint(
+    "admin",
+    __name__,
+    template_folder="templates",
+    static_folder="static",
+    url_prefix="/admin"
+)
+
+
+@admin.route("/")
+def dashboard():
+    return render_template("dashboard.html")
