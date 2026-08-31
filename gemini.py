af260d4b9df79748232877ab18f67fa870c363de
EpiksXX <didired284@gmail.com>
2026-08-31 23:10:55 +0300

    gemini.py

--- gemini.py
+++ gemini.py
@@ -4,7 +4,7 @@ from schemas import ChatRequest
 from admin.lore_engine import build_augmented_system_prompt
 
 # Всегда используем только эту модель
-MODEL = "gemini-3-flash-preview"
+MODEL = "gemini-3.1-flash-lite"
 
 
 def convert_messages(chat: ChatRequest):
